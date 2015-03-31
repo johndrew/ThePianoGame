ec1a6636a9ae7f31233120d880720154884b4796
Taylor Johndrew <johndrew.t@gmail.com>
2015-03-31 09:17:11 -0400

    First Commit. Setting up MVC framework.

---
+++ src/thepianogame/controller/MainController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thepianogame.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author taylorstheking4
+ */
+public class MainController extends JFrame implements ActionListener {
+    
+    public MainController() {
+        uiInit();
+    }
+    
+    /*
+        Setup the GUI and ActionListeners
+    */
+    public void uiInit() {
+        
+    }
+
+    /*
+        Used for responding to menu clicks
+    */
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/thepianogame/models/Piano.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thepianogame.models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author taylorstheking4
+ */
+public class Piano {
+    
+    private int keysTotal;
+    private ArrayList<PianoKey> keys;
+    
+    public Piano() {}
+}
