5e056528543168417f80174a8e26a75fafc5e180
Graham Ritter (scrittah.com) <graham@scrittah.com>
2015-04-01 13:22:17 -0400

    added function to return new (random) chords and a class for handling scales

--- src/thepianogame/models/Game.java
+++ src/thepianogame/models/Game.java
@@ -6,11 +6,21 @@
 package thepianogame.models;
 
 public class Game {
-    
+
+    static Scale major
+            = new Scale(new int[]{0, 2, 4, 5, 7, 9, 11}, new String[]{"major",
+                "minor", "minor", "major", "major", "minor", "diminished"});
+    static Scale minor
+            = new Scale(new int[]{0, 2, 3, 5, 7, 8, 10}, new String[]{"minor",
+                "diminished", "major", "minor", "minor", "major", "major"});
+    static final String[] keyNames
+            = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A",
+                "A#", "B"};
+
     public Game() {
         run();
     }
-    
+
     public void run() {
         // main game loop
         while (true) {
