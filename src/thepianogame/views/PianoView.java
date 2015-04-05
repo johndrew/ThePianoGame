db66c4a60474ba96754923f0a0ae0a2b6540345b
Marina Stoebner <marina.stoebner@gmail.com>
2015-04-04 22:19:43 -0400

    changed keyboard so that the size doesnt change but when in computer mode the keys will be labeled with their associated keys on the keyboard

--- src/thepianogame/views/PianoView.java
+++ src/thepianogame/views/PianoView.java
@@ -5,12 +5,14 @@
  */
 package thepianogame.views;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -36,23 +38,10 @@ public class PianoView extends JScrollPane {
         JLayeredPane layer = new JLayeredPane();
         layer.setSize(685, 92);
         int keyIndex = 0, i;
-        int totalKeys, totalWhiteKeys;
-        
-        // Change the amount of keys in the piano visualization based on
-        // if the MIDI keyboard is being used, or if the computer keyboard is
-        if (current_mode == MIDI_KEYBOARD) {
-            totalKeys = 53;
-            totalWhiteKeys = 31;
-        }
-        else {
-            totalKeys = 21;
-            totalWhiteKeys = 11;
-        }
-        
-        keys = new JPanel[totalKeys];
+        keys = new JPanel[53];
 
         if (current_mode == MIDI_KEYBOARD) {
-            for(i=0;i<totalWhiteKeys;i++){
+            for(i=0;i<31;i++){
                 keys[keyIndex] = createWhiteKey(i);
                 layer.add(keys[keyIndex], 0, -1);
                 keyIndex+=1;
@@ -67,30 +56,52 @@ public class PianoView extends JScrollPane {
             ArrayList<Integer> keyEvents = makeComputerKeyEventsList();
             computerKeyEventMap = new HashMap<Integer,Integer>();
             
-            for(i=0;i<totalWhiteKeys;i++) {
+            for(i=0;i<31;i++) {
                 keys[keyIndex] = createWhiteKey(i);
                 layer.add(keys[keyIndex], 0, -1);
-                computerKeyEventMap.put(keyEvents.get(keyIndex), keyIndex);
+                if(keyIndex > 16 && keyIndex < 36){
+                    computerKeyEventMap.put(keyEvents.get(keyIndex-17), keyIndex);
+                }
                 keyIndex+=1;
                 
                 // computer keyboard needs to start on an F and end on a B
-                if(i%7!=3 && i%7!=6){
+                if(i%7!=2 && i%7!=6){
                     keys[keyIndex] = createBlackKey(i);
                     layer.add(keys[keyIndex], 1, -1);
-                    computerKeyEventMap.put(keyEvents.get(keyIndex), keyIndex);
+                    if(keyIndex > 16 && keyIndex < 36){
+                        computerKeyEventMap.put(keyEvents.get(keyIndex-17), keyIndex);
+                    }
                     keyIndex+=1;
                 }
+                
             }
+            
+            labelWhiteKeys(keys);
+            labelBlackKeys(keys);
         }
-        
+     
         this.setViewportView(layer);
         setSize(685, 92); 
         
+    }
+  
+    private void createKeyboard(JLayeredPane layer, int numberOfKeys, int keyIndex, int i){
+        for(i=0;i<numberOfKeys;i++){
+                keys[keyIndex] = createWhiteKey(i);
+                layer.add(keys[keyIndex], 0, -1);
+                keyIndex+=1;
+                if(i%7!=2 && i%7!=6){
+                    keys[keyIndex] = createBlackKey(i);
+                    layer.add(keys[keyIndex], 1, -1);
+                    keyIndex+=1;
+                }
+            }
     }
     
     // creates the piano's white keys
     private JPanel createWhiteKey(int i){
         JPanel whiteKey = new JPanel();
+        whiteKey.setLayout(new BorderLayout());
         whiteKey.setBorder(javax.swing.BorderFactory.createBevelBorder(
                 javax.swing.border.BevelBorder.RAISED));
         whiteKey.setBackground(Color.WHITE);
@@ -102,10 +113,41 @@ public class PianoView extends JScrollPane {
     // creates the piano's black keys
     private JPanel createBlackKey(int i){
         JPanel blackKey = new JPanel();
+        blackKey.setLayout(new BorderLayout());
         blackKey.setBackground(Color.BLACK);
         blackKey.setLocation(15 + i*22,0);
         blackKey.setSize(15, 64);
         return blackKey;
+    }
+    
+    // if the game is in computer mode, it labels the white keys with the 
+    // associating letters on the keyboard
+    private void labelWhiteKeys(JPanel[] keys){
+        int[] whiteKeyIndexes = {0, 2, 4, 6, 7, 9, 11, 12, 14, 16, 18};
+        String[] whiteComputerKeys = {"A", "S", "D", "F", "G","H", "J", "K", "L", 
+            ";", "'"};
+       
+        
+        for(int i = 0; i < whiteKeyIndexes.length; i++){
+            JLabel label = new JLabel(whiteComputerKeys[i]);
+            label.setHorizontalAlignment(JLabel.CENTER);
+            keys[whiteKeyIndexes[i] + 17].add(label, BorderLayout.SOUTH);
+        }
+               
+    }
+    
+    // if the game is in computer mode, it labels the black keys with the 
+    // associating letters on the keyboard
+    private void labelBlackKeys(JPanel[] keys) {
+        int[] blackKeyIndexes = {1, 3, 5, 8, 10, 13, 15, 17};
+        String[] blackComputerKeys = {"W", "E", "R", "Y", "U", "O", "P", "["};
+        
+        for(int i = 0; i < blackKeyIndexes.length; i++){
+            JLabel label = new JLabel(blackComputerKeys[i]);
+            label.setForeground(Color.white);
+            label.setHorizontalAlignment(JLabel.CENTER);
+            keys[blackKeyIndexes[i] + 17].add(label, BorderLayout.SOUTH);
+        }
     }
     
     // takes in a set of key indexes mapping to the location of the keys on
@@ -168,7 +210,7 @@ public class PianoView extends JScrollPane {
         */
         keys[keyIndex].setBackground(color);
     }
-    
+
     public ArrayList<Integer> makeComputerKeyEventsList() {
         /*
             Creates a list of the keyEvents for the computer that will be used
@@ -217,8 +259,6 @@ public class PianoView extends JScrollPane {
     }
     
     public JPanel[] keys;
-    public int[] leftC = {12, 16, 19};
-    public int[] leftCInverted = {16, 19, 24};
     
     private Piano model;
     private CONTROL_MODE current_mode;
